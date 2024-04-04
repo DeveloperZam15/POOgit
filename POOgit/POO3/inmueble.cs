2c026e2688c8e33f4c21f63c0b9c6a136a79c202
Daniel Zamora <u13w00@gmail.com>
2024-04-04 12:49:46 -0600

    Actualización no recuerdo!

---
+++ POOgit/POO3/inmueble.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOgit.POO3
+{
+    internal class inmueble
+    {
+        //ATRIBUTOS
+
+        string tipoInmueble;
+
+        string ubicacion;
+
+        string area;
+
+        int cuartos;
+
+        int precioInmueble;
+        
+        //MÉTODOS
+        public void registrar() { }
+
+        public void vender() {
+            Console.WriteLine(tipoInmueble + " ha dido vendida por el precio de " + precioInmueble);
+        }
+
+        public void alquilar() {
+            Console.WriteLine(tipoInmueble + "ha dio alquilada por el precio de " + precioInmueble);
+        }
+
+        public void proyectoPreventa() { }
+
+        //MÉTODO CONSTRUCTOR CON PARAMETROS
+
+        public inmueble(string tipo, int precio) {
+            tipo = tipoInmueble;
+            precio = precioInmueble;
+
+        }
+    }
+}
--- POOgit/Program.cs
+++ POOgit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using POOgit.POO2;
+using POOgit.POO3;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,24 +8,33 @@ using System.Threading.Tasks;
 
 namespace POOgit
 {
-    internal class Program
+    internal class Program1
     {
         static void Main(string[] args)
         {
-            //Instancia/crea el objeto, llama al constructor
-            casa casaObjeto = new casa();
+            ////POO CASA ///////////////////////////////////////////////////////////////////
+            ////Instancia/crea el objeto, llama al constructor
+            //casa casaObjeto = new casa();
 
-            //NOTA: el constructor se crea automaticamente o por defecto si no se declara
+            ////NOTA: el constructor se crea automaticamente o por defecto si no se declara
 
-            //Se accede a un atributo y se le da un valor
-            casaObjeto.color = "Rojo";
+            ////Se accede a un atributo y se le da un valor
+            //casaObjeto.color = "Rojo";
 
-            //Se manda a llamar un método
-            casaObjeto.Venta();
+            ////Se manda a llamar un método
+            //casaObjeto.Venta();
 
-            //Se crea un nuevo objeto, se llama al constructor con atributos
-            casa casaObjetoDos = new casa("Marrón");
-            casaObjetoDos.Venta();
+            ////Se crea un nuevo objeto, se llama al constructor con atributos
+            //casa casaObjetoDos = new casa("Marrón");
+            //casaObjetoDos.Venta();
+            ////POO CASA ///////////////////////////////////////////////////////////////////
+            inmueble inm1 = new inmueble("Casa de playa", 150000);
+            inm1.vender();
+
+            inmueble inm2 = new inmueble("Casa de campo", 200000);
+            inm2.alquilar();
+
+
 
             Console.ReadKey();
         }
